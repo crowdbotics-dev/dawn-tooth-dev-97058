33183b17edf74f30bf0cb53817d881b1e487d3bc
Crowdbotics <engineering@crowdbotics.com>
2023-07-04 01:06:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dawn-tooth-dev-97058",
+  url: "https://dawn-tooth-dev-97058.botics.co",
+  api: "https://dawn-tooth-dev-97058.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
